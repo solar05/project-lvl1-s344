f9cbe2d682be509bdfc6cae54b2debcd7e5056e9
solar05 <solar7455@gmail.com>
2018-09-10 20:20:41 +0300

    Refactored asking function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import readlineSync from 'readline-sync';
-const userName = readlineSync.question('May I have your name?');
-console.log(`Hello, ${userName}`);
+
+const brainGames = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}.`);
+};
+export default brainGames;
