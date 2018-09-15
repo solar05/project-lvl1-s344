995a1ebc1f17dc4537d2d586a67e57f866a6341e
solar05 <solar7455@gmail.com>
2018-09-15 19:53:20 +0300

    Refactor brain-prime

--- README.md
+++ README.md
@@ -7,20 +7,20 @@
 [![Test Coverage](https://api.codeclimate.com/v1/badges/819a4f988cbc45c2a230/test_coverage)](https://codeclimate.com/github/solar05/project-lvl1-s344/test_coverage)
 
 
-##brain-even
+## brain-even
 https://asciinema.org/a/yVGqqo79n0s7SgIkYGqUBrUYF
 
-##brain-calc
+## brain-calc
 https://asciinema.org/a/KlXF0KDCyUamRMVKYrJRBl2Wu
 
-##brain-gcd
+## brain-gcd
 https://asciinema.org/a/K7hyXd7Ba32dMo0rNDwiVHqTu
 
-##brain-balance
+## brain-balance
 https://asciinema.org/a/Mgp64QWFmfSMF2WVzqPk7ehfA
 
-##brain-progression
+## brain-progression
 https://asciinema.org/a/2EwjWLe3XKBo2pdQDi3fY0dH2
 
-##brain-prime
+## brain-prime
 https://asciinema.org/a/QTIfQQaU7R96O3PgPet0hLTwT
--- src/games/prime.js
+++ src/games/prime.js
@@ -4,19 +4,20 @@ import getRandomNum from '../generateRandomNum';
 const rule = 'Is this number is prime?';
 
 const isPrime = (number) => {
-  if (number === 1) {
-    return false;
-  }
-  for (let divisor = 2; divisor < number; divisor += 1) {
-    if (number % divisor === 0) {
+  const iter = (num, divider) => {
+    if (num / 2 < divider) {
+      return true;
+    }
+    if (num % divider === 0) {
       return false;
     }
-  }
-  return true;
+    return iter(num, divider + 1);
+  };
+  return iter(number, 2);
 };
 
 const prime = () => {
-  const randommNum = getRandomNum(1, 100);
+  const randommNum = getRandomNum(2, 100);
   const data = {
     answer: isPrime(randommNum) ? 'yes' : 'no',
     question: randommNum,
